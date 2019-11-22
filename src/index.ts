4bd817b7f7a173966eef5d34d81847784c7bae09
Bart Wijnants <bart.w@sparkcentral.com>
2019-11-22 19:56:21 +0100

    prettify

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
